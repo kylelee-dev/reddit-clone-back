408b0d9e3b36bd6c5158965b8d9d698840e581fe
kylelee-dev <leekyle.dev@gmail.com>
2024-09-16 13:01:50 -0400

    Initialize server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+
+// HTTP request logger
+app.use(morgan("dev")); 
+
+app.get("/", (_, res) => res.send("running"));
+
+
+let port = 4000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
