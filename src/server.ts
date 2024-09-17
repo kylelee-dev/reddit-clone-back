c79f3dce93d616c26e3ca871363233cc3722a2b7
kylelee-dev <leekyle.dev@gmail.com>
2024-09-17 16:57:41 -0400

    Set up initial DB connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import { AppDataSource } from "./data-source";
 
 const app = express();
 
@@ -15,4 +16,10 @@ let port = 4000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
+
+    AppDataSource.initialize().then(async () => {
+
+        console.log("Database initialized...")
+    }).catch(error => console.log(error))
+    
 })
