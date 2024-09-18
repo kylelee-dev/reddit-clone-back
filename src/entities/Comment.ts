65376e10ee09347b1518b593a0ce2d8bdc8d9f6f
kylelee-dev <leekyle.dev@gmail.com>
2024-09-18 13:38:33 -0400

    Create entities [user,post,sub,comment,vote]

---
+++ src/entities/Comment.ts
@@ -0,0 +1,62 @@
+import {
+  BeforeInsert,
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+} from "typeorm";
+import BaseEntity from "./BaseEntity";
+import User from "./User";
+import { Post } from "./Post";
+import { Exclude, Expose } from "class-transformer";
+import Vote from "./Vote";
+import { makeId } from "../utils/helpers";
+
+@Entity("comments")
+export default class Comment extends BaseEntity {
+  @Index()
+  @Column()
+  identifier: string;
+
+  @Column()
+  body: string;
+
+  @Column()
+  username: string;
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "username", referencedColumnName: "username" })
+  user: User;
+
+  @Column()
+  postId: number;
+
+  @ManyToOne(() => Post, (post) => post.comments, { nullable: false })
+  post: Post;
+
+  @Exclude()
+  @OneToMany(() => Vote, (vote) => vote.comment)
+  votes: Vote[];
+
+  protected userVote: number;
+
+  setUserVote(user: User) {
+    const index = this.votes?.findIndex((v) => v.username === user.username);
+    this.userVote = index > -1 ? this.votes[index].value : 0;
+  }
+
+  @Expose() get voteScore(): number {
+    const initialValue = 0;
+
+    return this.votes?.reduce(
+      (prev, currObj) => prev + (currObj.value || 0),
+      initialValue
+    );
+  }
+
+  @BeforeInsert()
+  makeId() {
+    this.identifier = makeId(8);
+  }
+}
---
+++ src/entities/User.ts
@@ -0,0 +1,34 @@
+import { BeforeInsert, Column, Entity, Index, OneToMany } from "typeorm";
+import BaseEntity from "./BaseEntity";
+import { IsEmail, Length } from "class-validator";
+import { Post } from "./Post";
+import bcrypt from "bcryptjs";
+import Vote from "./Vote";
+
+@Entity("users")
+export default class User extends BaseEntity {
+  @Index()
+  @IsEmail(undefined, { message: "Email is invalid." })
+  @Length(1, 255, { message: "Email cannot be empty." })
+  @Column({ unique: true })
+  email: string;
+
+  @Index()
+  @Length(3, 32, { message: "Username must be at least 3 characters long." })
+  @Column()
+  username: string;
+
+  @Length(6, 32, { message: "Password must be at least 6 characters long." })
+  @Column()
+  password: string;
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
+
+  @OneToMany(() => Vote, (vote) => vote.user)
+  votes: Vote[];
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+}
