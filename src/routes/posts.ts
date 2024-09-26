0eaf66b63cacd6e1e2d30422aa8f0bb524932df9
kylelee-dev <leekyle.dev@gmail.com>
2024-09-26 14:21:36 -0400

    Handle createPostComment

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import Sub from "../entities/Sub";
 import Post from "../entities/Post";
+import Comment from "../entities/Comment";
 import userMiddleware from "../middlewares/user";
 import authMiddleware from "../middlewares/auth";
 
@@ -33,21 +34,47 @@ const createPost = async (req: Request, res: Response) => {
 };
 
 const getPost = async (req: Request, res: Response) => {
-    const {identifier, slug} = req.params;
-    
-    try {
-        const post = await Post.findOneOrFail({
-            where: {identifier, slug},
-            relations: ["sub", "votes"]
-        })
-        if (res.locals.user) post.setUserVote(res.locals.user);
-        return res.json(post)
-    } catch (error) {
-        console.log(error);
-        return res.status(404).json({error: "Post cannot be found."})
-    }
-}
+  const { identifier, slug } = req.params;
 
+  try {
+    const post = await Post.findOneOrFail({
+      where: { identifier, slug },
+      relations: ["sub", "votes"],
+    });
+    if (res.locals.user) post.setUserVote(res.locals.user);
+    return res.json(post);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ error: "Post cannot be found." });
+  }
+};
+const createPostComment = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params;
+  const { body: commentBody } = req.body;
+
+  try {
+    const post = await Post.findOneByOrFail({ identifier, slug });
+    const comment = new Comment();
+    comment.body = commentBody;
+    comment.user = res.locals.user;
+    comment.post = post;
+
+    if (res.locals.user) {
+      post.setUserVote(res.locals.user);
+    }
+
+    await comment.save();
+    return res.json(comment);
+  } catch (error) {
+    console.log(error)
+    return res.status(404).json({error: "Post cannot be found."})  }
+};
+router.post(
+  "/:identifier/:slug/comment",
+  userMiddleware,
+  authMiddleware,
+  createPostComment
+);
 router.post("/", userMiddleware, authMiddleware, createPost);
 router.get("/:identifier/:slug", userMiddleware, getPost);
 export default router;
